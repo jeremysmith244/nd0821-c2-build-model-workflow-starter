0a43bc0298e196ef7a7c1b55563b3bf7dc07d6cc
jeremysmith244 <jeremysmith244@gmail.com>
2022-01-28 22:04:36 -0800

    fixed bug, cleaning step is now pre file saving

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -27,23 +27,23 @@ def go(args):
     # YOUR CODE HERE     #
     ######################
 
-    logger.info("Downloading raw data artifact")
+    logger.info("Downloading raw data artifact: %s" % args.input_artifact)
     artifact_local_path = run.use_artifact(args.input_artifact).file()
     df = pd.read_csv(artifact_local_path, index_col="id")
 
-    logger.info("Processing raw data artifact")
+    logger.info("Processing raw data artifact: %s" % args.input_artifact)
     # Drop outliers
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
-    df.to_csv(args.output_artifact)
 
     # Drop longitude outside of bounds
     idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
+    df.to_csv(args.output_artifact)
 
-    logger.info("Creating clean artifact")
+    logger.info("Creating clean artifact: %s" % args.output_artifact)
     artifact = wandb.Artifact(
         args.output_artifact,
         type=args.output_type,
@@ -51,7 +51,7 @@ def go(args):
     )
     artifact.add_file(args.output_artifact)
 
-    logger.info("Logging artifact")
+    logger.info("Logging artifact: %s" % args.output_artifact)
     run.log_artifact(artifact)
 
 if __name__ == "__main__":
